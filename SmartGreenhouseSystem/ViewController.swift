78fba560f4305b4ae433c366cb6a5a58ae9783ce
Murat Can <muratcanqaslan@gmail.com>
2020-11-21 17:27:43 +0300

    Initial Commit

---
+++ SmartGreenhouseSystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartGreenhouseSystem
+//
+//  Created by Murat Can on 21.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
